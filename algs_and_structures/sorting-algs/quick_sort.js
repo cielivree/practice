077c83af879a8516639d2b7f9d4830d6824e6021
cielivree <cielivree93@gmail.com>
2025-07-02 19:30:43 +0300

    Quick sort

--- algs_and_structures/sorting-algs/quick_sort.js
+++ algs_and_structures/sorting-algs/quick_sort.js
@@ -5,9 +5,24 @@ function quickSort(array) {
         return array
     }
 
-    let index = Math.floor(array.length / 2)
-    let el = array[index]
+    let middle = Math.floor(array.length / 2)
+    let el = array[middle]
     let less = []
     let bigger = []
 
+    for (let i = 0; i < array.length; i++) {
+        if (i === middle) {
+            continue
+        }
+
+        if (el > array[i]) {
+            less.push(array[i])
+        } else {
+            bigger.push(array[i])
+        }
+    }
+
+    return [...quickSort(less), el, ...quickSort(bigger)]
 }
+
+console.log(quickSort(arr))   // [-5, -1, 0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4, 4, 5, 6, 6, 6, 6, 7, 8, 9, 9, 23, 32, 35]
