d84cccbcfe7d7b78adad88b1a6af902ab6c5a511
cielivree <cielivree93@gmail.com>
2025-07-02 19:18:27 +0300

    Quick sort

--- algs_and_structures/sorting-algs/quick_sort.js
+++ algs_and_structures/sorting-algs/quick_sort.js
@@ -1,1 +1,13 @@
 const arr = [0, 3, 2, 5, 6, 8, 1, 9, 4, 2, 1, 2, 9, 6, 4, 1, 7, -1, -5, 23, 6, 2, 35, 6, 3, 32]
+
+function quickSort(array) {
+    if (array.length <= 1) {
+        return array
+    }
+
+    let index = Math.floor(array.length / 2)
+    let el = array[index]
+    let less = []
+    let bigger = []
+
+}
