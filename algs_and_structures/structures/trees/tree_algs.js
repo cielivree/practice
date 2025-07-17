a19ea7602c55b3bda34d894db7c1c4ff30631019
cielivree <cielivree93@gmail.com>
2025-07-17 13:23:58 +0300

    Iteration function for tree

--- algs_and_structures/structures/trees/tree_algs.js
+++ algs_and_structures/structures/trees/tree_algs.js
@@ -57,3 +57,26 @@ const recursion = (tree) => {
 }
 
 console.log(recursion(tree))   // 70
+
+const iteration = (tree) => {
+    if (!tree.length) {
+        return 0;
+    }
+
+    let sum = 0
+    let stack = []
+    tree.forEach(node => stack.push(node))
+
+    while (stack.length) {
+        const node = stack.pop()
+        sum += node.v
+        
+        if (node.c) {
+            node.c.forEach(child => stack.push(child)) 
+        }
+    }
+
+    return sum
+}
+
+console.log(iteration(tree)) // 
