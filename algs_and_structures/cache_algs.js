9857eeef7cc1cd28098ba4bbf4e5032a3fdd685a
cielivree <cielivree93@gmail.com>
2025-07-18 11:38:44 +0300

    Cache function

--- algs_and_structures/cache_algs.js
+++ algs_and_structures/cache_algs.js
@@ -1,5 +1,15 @@
 function cacheFunction(fn) {
+    const cache = {}
 
+    return function(n) {
+        if (cache[n]) {
+            return cache[n]  // Pulling from cache 
+        }
+
+        let result = fn(n)  // The function calculate
+        cache[n] = result
+        return result
+    }
 }
 
 function factorial(n) {
