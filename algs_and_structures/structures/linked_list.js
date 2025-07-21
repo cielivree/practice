52eb5a9363659e938f5b2d296b5258dabd9fab2d
cielivree <cielivree93@gmail.com>
2025-07-21 12:46:23 +0300

    Linked list (add value)

--- algs_and_structures/structures/linked_list.js
+++ algs_and_structures/structures/linked_list.js
@@ -6,8 +6,19 @@ class LinkedList {
 
     add(value) {
         if (this.size === 0) {
-            
+            this.root = new Node(value) 
+            this.size += 1
+            return true
         }
+
+        let node = this.root
+        while (node.next) {
+            node = node.next
+        }
+
+        let newNode = new Node(value)
+        value.next = newNode
+        this.size += 1
     }
 }
 
