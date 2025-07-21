d7a16c490bf00694b3cf079e811e503c17005dc8
cielivree <cielivree93@gmail.com>
2025-07-21 12:35:57 +0300

    Linked lists

---
+++ algs_and_structures/structures/linked_list.js
@@ -0,0 +1,19 @@
+class LinkedList {
+    constructor() {
+        this.size = 0
+        this.root = null
+    }
+
+    add(value) {
+        if (this.size === 0) {
+            
+        }
+    }
+}
+
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
