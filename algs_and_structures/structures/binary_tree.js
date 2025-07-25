a09fc4c6730c1899a59124fa27c1381339cd4d29
cielivree <cielivree93@gmail.com>
2025-07-25 07:19:40 +0300

    Add function

--- algs_and_structures/structures/binary_tree.js
+++ algs_and_structures/structures/binary_tree.js
@@ -2,6 +2,35 @@ class BinaryTree {
     constructor() {
         this.root = null
     }
+
+    add(value) {
+        if (!this.root) {
+            this.root = new TreeNode(value)
+        } else {
+            let node = this.root
+            let newNode = new TreeNode(value)
+            while (node) {
+                if (value > node.value) {
+                    if (!node.right) {
+                        break
+                    }
+                    node = node.right
+                } else {
+                     if (!node.left) {
+                        break
+                    }
+                    node = node.left
+                }
+            }
+
+            if (value > node.value) {
+                node.right = newNode
+            } else {
+                node.left = newNode
+            }
+
+        }
+    }
 }
 
 class TreeNode {
