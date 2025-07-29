7f8200045f06b02593434a90f7e6a0c0cfd16c79
cielivree <cielivree93@gmail.com>
2025-07-29 08:47:04 +0300

    Set, map

--- algs_and_structures/structures/set_map.js
+++ algs_and_structures/structures/set_map.js
@@ -1,5 +1,16 @@
 const map = new Map()
 
 map.set("name", "Javascript")
+console.log(map.get("name"))   // Javascript
 
-console.log(map.get("name"))
+const set = new Set()
+
+set.add(5)
+set.add(5)
+set.add(5)
+set.add(5)
+set.add(4)
+set.add(1)
+set.add(3)
+
+console.log(set)   // Set(4) {5, 4, 1, 3}
